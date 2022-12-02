be73f09a98bc7b289b99433fe9e0785524bc8963
Al Riad Hridoy <64416616+AlRiadHridoy@users.noreply.github.com>
2022-12-02 11:52:43 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import SinglePhoto from "./components/SinglePhoto.js";
 
 
 function App() {
-  const { theme, query } = useGlobalContext();
+  const { theme } = useGlobalContext();
   return (
     <main className={`root ${theme}`}>
       <div
